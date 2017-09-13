0a069e2bc3cdc62c3fcf1f3283f30946bc86e07b
Adolfo Bernal <31907186+adbernal@users.noreply.github.com>
2017-09-13 10:21:39 -0300

    Carga de archivos
    Es la primera carga de archivos

---
+++ login.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Clientes_Web
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_login_Click(object sender, EventArgs e)
+        {
+            if(txt_user.Text == "usuario" && txt_pass.Text == "123")
+            {
+                Response.Redirect("Menu.aspx");
+                
+            }else
+            {
+                lbl_error.Text = "ERROR DE USUARIO";
+            }
+        }
+    }
+}
